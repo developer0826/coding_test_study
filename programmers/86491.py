280793ac3fc9bd7fc18baf5e60fffcbfc5b96917
GU Seona <seona_0826@naver.com>
2024-06-21 17:20:10 +0900

    240621_sa_add_commentary

--- programmers/86491.py
+++ programmers/86491.py
@@ -5,11 +5,18 @@ def solution(sizes):
     widths = []
     heights = []
 
+    # sizes로 들어온 가로, 세로값을 받아
+    # 긴 값이 가로(너비)값이 될 수 있도록 기준 잡아 정렬
     for w, h in sizes:
         if w < h:
             w, h = h, w
+        
+        # 가로/세로를 별도의 리스트에 담는다.
         widths.append(w)
         heights.append(h)
     
+    # 지갑의 너비는 너비가 가장 긴 카드의 너비
+    # 지갑의 높이는 높이가 가장 긴 카드의 높이여야
+    # 모든 카드를 담을 수 있다.
     answer = max(widths) * max(heights)
     return answer
